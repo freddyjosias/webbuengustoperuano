b9f4db2e7ecea6e8bda47bb9fab5f2e84faf8879
arianjosafat67 <arianjosafat67@gmail.com>
2020-09-19 13:05:59 -0500

    Logo.ico

--- index.php
+++ index.php
@@ -11,6 +11,8 @@
 <head>
 	<title>El Buen Gusto Peruano</title>
     <meta charset="utf-8">
+    <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/normalize.css">
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
