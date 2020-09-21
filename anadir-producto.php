b8bf6dfe1310a5037401f06985dbe644b90236e1
Freddy Josias Herrera Becerra <gerson123tuesta@gmail.com>
2020-09-21 07:12:44 -0500

    f

---
+++ anadir-producto.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <title>Quienes Somos - Restaurante 1</title>
+    <link rel="shorcut icon" href="img/favicon.ico">
+    <link rel="stylesheet" href="css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+</head>
+<body>
+
+    <main>
+        <div class="contenedor-general panel-control">
+            <nav>
+                <ul>
+                    <li><a href="panel.php">Inicio</a></li>
+                    <li><a href="">Añadir Producto</a></li>
+                    <li><a href="">Eliminar Producto</a></li>
+                    <li><a href="">Actualizar Praducto</a></li>
+                </ul>
+            </nav>
+
+            <div>
+                <h1>Añadir Producto</h1>
+                <form action="">
+                    <form action="">
+                    <p>Producto:<input type="text" placeholder="Producto">
+                    </p>  
+                    </form>
+                    
+                    <form action="">
+                        <p>Precio: <input type="number" placeholder="Precio">
+                        </p>
+                    </form>
+                    
+                    <form action="">
+                    <p>Cantidad: <input type="number" placeholder="Cantidad">
+                    </p>
+                    </form>
+
+                    <input type="submit" value="Añadir">
+                </form>
+            </div>
+
+        </div>
+    </main>
+
+</body>
+</html>
--- panel.php
+++ panel.php
@@ -13,33 +13,19 @@
 
     <main>
         <div class="contenedor-general panel-control">
-            <nav class=>
+            <nav>
                 <ul>
-                    <li>Anñadir Producto</li>
-                    <li>Añadir Forma de Pago</li>
-                    <li>Añador Tipo de envio</li>
+                    <li><a href="">Inicio</a></li>
+                    <li><a href="anadir-producto.php">Añadir Producto</a></li>
+                    <li><a href="">Eliminar Producto</a></li>
+                    <li><a href="">Actualizar Praducto</a></li>
                 </ul>
             </nav>
 
-            <div class="prueba1">
-                <img src="img/norteño.jpg" class="img-panel-control">
-                <div class="bloque-panel-control">
-                    <div class="bloques-panel-control">
-                        <a href="">asdasd</a>
-                    </div>
-
-                    <div class="bloques-panel-control">
-                        <a href="">asdsadf</a>
-                    </div>
-
-                    <div class="bloques-panel-control">
-                        <a href="">asddasc</a>
-                    </div>
-                    
-                    <div class="bloques-panel-control">
-                        <a href="">asdasadas</a>
-                    </div>
-                </div> 
+            <div class='contenido-panel-home'>
+                <img src="img/logo-icono.png">
+                <p>Bienvenido a su Panel de Control</p>
+                <a href="">Ver mi Restaurante</a>
             </div>
 
         </div>
