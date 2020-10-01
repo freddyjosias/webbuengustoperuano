12f47efa1d730a4309a2e9d037dd27837c9fbe97
Freddy Josias Herrera Becerra <gerson123tuesta@gmail.com>
2020-09-30 19:06:44 -0500

    f

--- index.php
+++ index.php
@@ -13,6 +13,7 @@
 <head>
 	<title>El Buen Gusto Peruano</title>
     <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shorcut icon" href="img/favicon.ico">
     <link href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/normalize.css">
