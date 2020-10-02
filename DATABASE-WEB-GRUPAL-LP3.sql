164513b8e9bbd26c5ae6a2c5dd9b1d30f657abcf
blaskho1 <idrox2811@gmail.com>
2020-10-02 16:56:15 -0500

    databaxez

--- DATABASE-WEB-GRUPAL-LP3.sql
+++ DATABASE-WEB-GRUPAL-LP3.sql
@@ -126,15 +126,21 @@ INSERT INTO sucursal (nomsucursal,direcsucursal,telefono,imgbienvenida,textobien
 VALUES ('La Collpa','Av. Circunvalación 202, Tarapoto','+51 42 522644','img/logocollpa.jpg','Cocina rica, hecha con mucha ilusión, para gente que le gusta comer y disfrutar cada bocado de la vida…..
 Cinco Sentidos es un lugar íntimo en el que puedes saborear a gusto y conversar de forma relajada. 
 Hacemos todo de forma casera y procuramos poner siempre un toque personal en nuestro trabajo diario, por eso somos un restaurante artesanal.
-Tenemos una bonita barra para tomar tapas recién hechas, y un salón con siete mesas en el que puedes sentarte a comer tranquilamente. A mediodía es un sitio informal con menú del día, por las noches es una sala de luces bajas y velas en las mesas… Por eso dicen que somos un restaurante con encanto.','img/juane.jpg','Juane de Gallina','img/inchicapi.jpg','Inchicapi de Gallina','img/arrozcaufa.jpg','Arroz Chaufa Regional','07:00','21:00','lacollpa@gmail.com');
+Tenemos una bonita barra para tomar tapas recién hechas, y un salón con siete mesas en el que puedes sentarte a comer tranquilamente. A mediodía es un sitio informal con menú del día, por las noches es una sala de luces bajas y velas en las mesas… Por eso dicen que somos un restaurante con encanto.',
+'img/juane.jpg','Juane de Gallina: El Juane​ es uno de los principales platos típicos de la gastronomía de la selva peruana y es muy consumido durante la fiesta de San Juan que se celebra el 24 de junio de cada año en honor a Juan Bautista',
+'img/inchicapi.jpg','Inchicapi de Gallina:El inchicapi de gallina es uno de los platos más tradicionales de la selva peruana. Su nombre proviene de los vocablos quechua “Inchik” que significa maní y “api” que quiere decir sopa.',
+'img/arrozchaufa.jpg','Arroz Chaufa Regional:Es un plato estilo chifa, una cocina china. Consiste en una mezcla de arroz frito con vegetales, que generalmente incluye cebolletas, huevos y pollo, cocinados rápidamente a fuego alto, a menudo en un wok con salsa de soja y aceite.','07:00','21:00','lacollpa@gmail.com');
 
 INSERT INTO sucursal (nomsucursal,direcsucursal,telefono ,imgbienvenida,textobienvenida, imgdestacado1,platodestacado1,imgdestacado2,platodestacado2,imgdestacado3,platodestacado3,horaatencioninicio,horaatencioncierre,correosucursal) 
-VALUES ('Doña Zully','Jr. San Pablo de la Cruz 244, Tarapoto','+51 42 530670','img/dona.jpg','Modernos en el estilo y clásicos en el sabor.
+VALUES ('Doña Zully','Jr. San Pablo de la Cruz 244, Tarapoto','+51 42 530670','img/doña.jpg','Modernos en el estilo y clásicos en el sabor.
  Un equipo de profesionales que hemos creado el restaurante donde nos gustaría comer a diario y en las ocasiones especiales. Con menú o a la carta. Con amigos o con clientes, con tiempo para disfrutar o con algo más de prisa porque el trabajo lo requiere.
  Firmes defensores de que calidad no está en el precio, sino en el producto.
  Exigentes porque también somos consumidores y estamos convencidos de que la experiencia debe resultar completa.
  Un buen restaurante ha de serlo por la comida, pero también por el trato y el entorno, por la decoración y el ambiente.
- Somos nuevos, pero también somos expertos. Natural Lunch, nuestro restaurante en La Finca (Pozuelo), nos ha permitido crecer y creer. Sabemos más y queremos demostrarlo','img/sudado.jpg','Sudado de Pescado','img/mixto.jpg','Mixto Regional','img/pescadofrito.jpg','Pescado Frito','08:00','20:00','doñazully@gmail.com');
+ Somos nuevos, pero también somos expertos. Natural Lunch, nuestro restaurante en La Finca (Pozuelo), nos ha permitido crecer y creer. Sabemos más y queremos demostrarlo',
+ 'img/sudado.jpg','Sudado de Pescado: El sudado de pescado es una sopa típica de la gastronomía peruana, aunque también se encuentra recogida en las cocinas de otros países latinoamericanos.',
+ 'img/mixto.jpg','Mixto Regional: Combinacion de varios platillos tradicionales.',
+ 'img/pescadofrito.jpg','Pescado Frito: Este platillo es altamente nutritivo y de agradable aroma.','08:00','20:00','doñazully@gmail.com');
 
 INSERT INTO sucursal (nomsucursal,direcsucursal,telefono ,imgbienvenida,textobienvenida, imgdestacado1,platodestacado1,imgdestacado2,platodestacado2,imgdestacado3,platodestacado3,horaatencioninicio,horaatencioncierre,correosucursal) 
 VALUES ('Chalet Venzia','Jr. Alegría Arias de Morey 293-175, Tarapoto','+51 42 522104','img/chalet.jpg','La Gastronomía peruana es reconocida a nivel mundial; con más de 250 platos típicos, se ha posicionado como una de las más importantes del planeta.
@@ -142,7 +148,10 @@ Sus preparaciones, sabores y  aromas son únicos y datan de añosas recetas. Es 
 Fue durante 2017, cuando “Perú Gastronómico” se consolida renovando su carta y su local; actualmente atiende de de lunes a domingo y ofrece servicio de cafetería, menú ejecutivo y a la carta y por cierto, una amplia gama de platos peruanos que día a día fascinan a quienes degustan estas preparaciones.
 Es la calidad en los productos utilizados y, por cierto, la destreza de nuestros chefs, la que otorga un producto de la más alta calidad, excelente presentación y exquisito sabor.
 El pisco sour, elaborado con la típica receta peruana, es el favorito de muchos, además de los postres hechos en el mismo restaurant.
-La invitación es a conocernos y a vivir un verdadero festín al paladar.','img/pescado.jpg','Pescado a la Hoja','img/tacacho.jpg','Tacaho con Cecina','img/caldo.jpg','Caldo de Gallina','07:00','21:00','chaletvenzia@gmail.com');
+La invitación es a conocernos y a vivir un verdadero festín al paladar.',
+'img/pescado.jpg','Pescado a la Hoja:El misterio en hornear el pescado en hojas de plátanos, viene del secreto para que el pescado no se queme al momento de cocinarse y para que también los sabores se vean resaltados con los sabores de la hoja.',
+'img/tacacho.jpg','Tacaho con Cecina:El tacacho con cecina es un plato típico de la gastronomía del Perú popular en la selva peruana​​ y ampliamente difundido en el resto del país.​',
+'img/caldo.jpg','Caldo de Gallina:El caldo de pollo es una sopa.​ A menudo se sirve con trozos de carne o con granos de arroz, etc. Se considera también un remedio casero contra los resfríos.','07:00','21:00','chaletvenzia@gmail.com');
 
 
 -- Ingreso de formas de pago
