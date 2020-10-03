996c0ee5cb44df42a17d7d8cce369593968e9bfd
arianjosafat67 <arianjosafat67@gmail.com>
2020-10-02 19:05:03 -0500

    formaspagoarreglado

--- formaspago.php
+++ formaspago.php
@@ -14,17 +14,17 @@
         
         if (isset($_POST['forma1'])) {
             $resultados1 = $conexion -> prepare('UPDATE detalleformaspago SET disponibilidadformaspago = ? WHERE idformaspago = 1 AND idsucursal = ?');
-            $resultados1 -> execute(array($_POST['forma1']), $idSucursal);
+            $resultados1 -> execute(array($_POST['forma1'], $idSucursal));
         }
 
         if (isset($_POST['forma2'])) {
             $resultados2 = $conexion -> prepare('UPDATE detalleformaspago SET disponibilidadformaspago = ? WHERE idformaspago = 2 AND idsucursal = ?');
-            $resultados2 -> execute(array($_POST['forma2']), $idSucursal);
+            $resultados2 -> execute(array($_POST['forma2'], $idSucursal));
         }
 
         if (isset($_POST['forma3'])) {
             $resultados3 = $conexion -> prepare('UPDATE detalleformaspago SET disponibilidadformaspago = ? WHERE idformaspago = 3 AND idsucursal = ?');
-            $resultados3 -> execute(array($_POST['forma3']), $idSucursal);
+            $resultados3 -> execute(array($_POST['forma3'], $idSucursal));
         }
     }
 
