8023e700ac53c0188764bff06d1ea797669e8183
blaskho1 <idrox2811@gmail.com>
2020-10-12 15:58:59 -0500

    conexion

--- conexion.php
+++ conexion.php
@@ -2,9 +2,9 @@
 
     try {
         $conexion = new PDO(
-            'mysql:host=bm12bwjh2ogki5nep1dq-mysql.services.clever-cloud.com;dbname=bm12bwjh2ogki5nep1dq',
-            'udq5trupmgax8sb2',
-            '2zEPbb8IUZ93mDSBLxwx'
+            'mysql:host=localhost;dbname=buengustoperuano',
+            'root',
+            ''
         );
     } catch (Exception $e) {
         echo $e -> getMessage();
