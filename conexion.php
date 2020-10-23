6ba62a576806703edd51cf4c279148ffe5705150
freddyjosias <fjosiashb@gmail.com>
2020-10-23 13:54:33 -0500

    f

--- conexion.php
+++ conexion.php
@@ -2,9 +2,9 @@
 
     try {
         $conexion = new PDO(
-            'mysql:host=localhost;dbname=buengustoperuano',
-            'root',
-            ''
+            'mysql:host=bm12bwjh2ogki5nep1dq-mysql.services.clever-cloud.com;dbname=bm12bwjh2ogki5nep1dq',
+            'udq5trupmgax8sb2',
+            '2zEPbb8IUZ93mDSBLxwx'
         );
     } catch (Exception $e) {
         echo $e -> getMessage();
