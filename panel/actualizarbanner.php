c8edcf152c5a590f71e8c89b00c3783637b220c4
larry1728 <harrietluna17@gmail.com>
2020-11-04 10:20:07 -0500

    xd

--- panel/actualizarbanner.php
+++ panel/actualizarbanner.php
@@ -1,6 +1,6 @@
 <?php
 
-    require 'conexion.php';
+    require '../conexion.php';
 
     session_start();
 
@@ -38,11 +38,11 @@
 	<meta charset="utf-8">
 	<link href="https://fonts.googleapis.com/css2?family=Dosis:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <title>Actualizar Banner</title>
-    <link rel="shorcut icon" href="img/favicon.ico">
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="css/bootstrap.add.css">
-    <link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+    <link rel="shorcut icon" href="../img/favicon.ico">
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.add.css">
+    <link rel="stylesheet" href="../css/normalize.css">
+    <link rel="stylesheet" type="text/css" href="../css/estilos.css">
 </head>
 <body>
 
@@ -82,7 +82,7 @@
                 </form>
 
                 <div class='text-center mt-5'>
-                    <?php echo "<img class='h-24r border border-dark' src='". $resultadosBanner[0]['banner'] ."' >" ?>
+                    <?php echo "<img class='h-24r border border-dark' src='../". $resultadosBanner[0]['banner'] ."' >" ?>
                 </div>
 
             </div>
