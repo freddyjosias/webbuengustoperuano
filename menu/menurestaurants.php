e78b319976a63b9a9eb6b7612261dbabf248cae1
freddyjosias <fjosiashb@gmail.com>
2020-11-03 22:23:47 -0500

    f

--- menu/menurestaurants.php
+++ menu/menurestaurants.php
@@ -21,13 +21,19 @@
 <nav class="navbar-restaurant navbar navbar-expand-lg navbar-light d-block d-md-none p-0">
     <div class="contenedor-general">
         <div class="container w-100 d-flex p-0 m-0 mw-100">
+
             <div class='logo-icono d-block d-md-none position-static'>
                 <a href="index.php"><img src="img/logo-icono.png" alt=""></a>
             </div>
 
-            <button class="navbar-toggler w-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="text-white"><i class="fas fa-bars"></i></span>
-            </button>
+            <div class='h-100 align-items-center'>
+                <div class="container m-0 p-0">
+                    <p class="navbar-toggler w-auto no-select p-0 m-0" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="text-white fw-600">MENÚ &nbsp; <i class="fas fa-bars"></i></span>
+                    </p>
+                </div>
+            </div>
+            
         </div>
         
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
