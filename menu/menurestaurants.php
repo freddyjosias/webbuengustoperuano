d98191ca6f685a6417aca5d7a70affe6c49f4ccd
freddyjosias <fjosiashb@gmail.com>
2020-11-03 22:00:10 -0500

    f

--- menu/menurestaurants.php
+++ menu/menurestaurants.php
@@ -1,12 +1,12 @@
 <div class="container-fluid p-0 d-none d-md-block">
     <nav class='nav-restaurant'>
         <ul>
-            <li><a href="bienvenida.php?view=<?php echo $idRestaurante ?>" class="op0">Bienvenida</a></li>
-            <li><a href="hacerpedido.php?view=<?php echo $idRestaurante ?>" class="op1">Pedidos</a></li>
-            <li><a href="nosotros.php?view=<?php echo $idRestaurante ?>" class="op2">Nosotros</a></li>
+            <li><a href="bienvenida.php?view=<?php echo $idRestaurante ?>" class="op0 h-100">Bienvenida</a></li>
+            <li><a href="hacerpedido.php?view=<?php echo $idRestaurante ?>" class="op1 h-100">Pedidos</a></li>
+            <li><a href="nosotros.php?view=<?php echo $idRestaurante ?>" class="op2 h-100">Nosotros</a></li>
                 <?php if ($_SESSION['profile'] == 2 && isset($_SESSION['sucursal'])) { ?>
                     <?php if ($_SESSION['sucursal'] == $_GET['view']) { ?>
-                        <li><a href="panel.php">Panel</a></li>
+                        <li><a href="panel.php" class='h-100'>Panel</a></li>
                     <?php } ?> 
                 <?php } ?> 
             <li>
