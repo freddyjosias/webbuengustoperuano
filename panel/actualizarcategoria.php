f00d66ca6be8e649c5e1c80fb16b56bd789de394
larry1728 <harrietluna17@gmail.com>
2020-11-04 23:54:07 -0500

    xd

--- panel/actualizarcategoria.php
+++ panel/actualizarcategoria.php
@@ -46,24 +46,8 @@
 
     <main>
         <div class="contenedor-general panel-control">
-            <nav>
-                <ul>
-                    <li><a href="panel.php">Inicio</a></li>
-                    <li><a href="restaurante.php">Restaurante</a></li>
-                    <li><a href="actualizarbanner.php">Actualizar Banner</a></li>
-                    <li><a href="actualizartextobienvenida.php">Actualizar Texto de Bienvenida</a></li>
-                    <li><a href="actualizarimagenbienvenida.php">Actualizar Imagen de Bienvenida</a></li>
-                    <li><a href="actualizardestacados.php">Actualizar Platos Destacados</a></li>
-                    <li><a href="anadircategoria.php">Añadir Categoria</a></li>
-                    <li><a href="eliminarcategoria.php">Eliminar Categoria</a></li>
-                    <li><a href="actualizarcategoria.php">Actualizar Categoria</a></li>
-                    <li><a href="anadirproducto.php">Añadir Producto</a></li>
-                    <li><a href="eliminarproducto.php">Eliminar Producto</a></li>
-                    <li><a href="actualizarproducto.php">Actualizar Praducto</a></li>
-                    <li><a href="actualizarformaspago.php">Actualizar Formas de Pago</a></li>
-                    <li><a href="actualizartipospedido.php">Actualizar Tipos de pedido</a></li>
-                </ul>
-            </nav>
+
+            <?php require '../menu/menupanel.php'; ?>
 
             <div class='formulario-panel'>
 
