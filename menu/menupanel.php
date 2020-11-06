a200682b90321a1ea76ffffc050702882297c44f
larry1728 <harrietluna17@gmail.com>
2020-11-05 19:59:37 -0500

    xd

--- menu/menupanel.php
+++ menu/menupanel.php
@@ -2,14 +2,14 @@
 
     <nav class='nav-panel-options'>
             <ul class='fw-600 ls-13'>
-                <li><a class='pl-5' href="../panel.php">Inicio</a></li>
+                <li><a class='pl-5' href="panel.php">Inicio</a></li>
                 <li><a class='pl-5' href="restaurante.php">Restaurante</a></li>
                 <li><a class='pl-5' href="actualizarbanner.php">Actualizar Banner</a></li>
                 <li><a class='pl-5' href="actualizartextobienvenida.php">Actualizar Texto de Bienvenida</a></li>
                 <li><a class='pl-5' href="actualizarimagenbienvenida.php">Actualizar Imagen de Bienvenida</a></li>
                 <li><a class='pl-5' href="actualizardestacados.php">Actualizar Platos Destacados</a></li>
-                <li><a class='pl-5' href="../categorias/listar.php">Categorias</a></li>
-                <li><a class='pl-5' href="../productos/listar.php">Productos</a></li>
+                <li><a class='pl-5' href="categorias/listar.php">Categorias</a></li>
+                <li><a class='pl-5' href="productos/listar.php">Productos</a></li>
                 <li><a class='pl-5' href="actualizarformaspago.php">Actualizar Formas de Pago</a></li>
                 <li><a class='pl-5' href="actualizartipospedido.php">Actualizar Tipos de pedido</a></li>
             </ul>
