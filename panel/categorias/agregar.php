d331a8dea55799032217653000219df01618f33a
larry1728 <harrietluna17@gmail.com>
2020-11-09 00:17:37 -0500

    xd

--- panel/categorias/agregar.php
+++ panel/categorias/agregar.php
@@ -46,15 +46,15 @@
 <body>
 
     <main>
-        <div class="contenedor-general panel-control">
+        <div class="container-fluid panel-control mw-1920p p-0">
 
-            <?php require '../../menu/menupanel.php'; ?>
+        <?php require '../../menu/menupanel.php'; ?>
 
-            <div class='formulario-panel container'>
+            <div class='formulario-panel container p-0 main-panel m-0 mw-85 w-85'>
 
-                <h1>Añadir Categoria</h1>
+                <h1 class='h3 text-center mt-5 font-weight-bold w-100'>Agregar Categoría</h1>
 
-                <form class='form-panel' method="post">
+                <form class='form-panel mt-5' method = "post">      
 
                     <p>Categoria Nueva: <input type="text" name='nuevacategoria'></p>  
 
