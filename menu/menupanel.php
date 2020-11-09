f30fd3597484b97d51d0b0bc6cce96c1413c0b4a
larry1728 <harrietluna17@gmail.com>
2020-11-08 22:12:43 -0500

    xd

--- menu/menupanel.php
+++ menu/menupanel.php
@@ -4,9 +4,7 @@
             <ul class='fw-600 ls-13'>
                 <li><a class='pl-5' href="panel.php">Inicio</a></li>
                 <li><a class='pl-5' href="restaurante.php">Información Restaurante</a></li>
-                <li><a class='pl-5' href="actualizarbanner.php">Actualizar Banner</a></li>
                 <li><a class='pl-5' href="bienvenida/bienvenida.php">Bienvenida</a></li>
-                <li><a class='pl-5' href="actualizarimagenbienvenida.php">Actualizar Imagen de Bienvenida</a></li>
                 <li><a class='pl-5' href="destacados/listar.php">Platos Destacados</a></li>
                 <li><a class='pl-5' href="categorias/listar.php">Categorias</a></li>
                 <li><a class='pl-5' href="productos/listar.php">Productos</a></li>
