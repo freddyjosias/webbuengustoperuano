1ee84adbde54f326942a0d982ba26071390f23b2
freddyjosias <fjosiashb@gmail.com>
2020-11-09 10:59:22 -0500

    f

--- loginuri.php
+++ loginuri.php
@@ -5,10 +5,35 @@
 
     $client = new Google_Client();
     
-    $client -> setClientId();
-    $client -> setClientSecret();
-    $client -> setRedirectUrl();
+    $client -> setClientId('252426069950-jp72flosvijj2su36e58bail2qmmk8nu.apps.googleusercontent.com');
+    $client -> setClientSecret('KcUczw-8uH_WVSgm8-qUE2bp');
+    $client -> setRedirectUri('http://localhost/webbuengustoperuano/loginuri.php');
     $client -> addScope('email');
     $client -> addScope('profile');
+
+    if (isset($_GET['code'])) 
+    {
+        $token =  $client->fetchAccessTokenWithAuthCode($_GET['code']);
+        
+        if (!isset($token['error'])) {
+
+            $client -> setAccessToken($token['access_token']);
+            $google_oauth = new Google_Service_Oauth2($client);
+            $google_account_info = $google_oauth -> userinfo -> get();
+            var_dump($google_account_info); die;
+
+        }
+
+    } 
+    else 
+    {
+        
+        ?>
+                <a href="<?php echo $client -> createAuthUrl() ?>">Google</a>
+            
+
+        <?php
+
+    }
 
 ?>
