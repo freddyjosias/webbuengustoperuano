8096ba563e375bd84c500a071ea56061a8576b3b
blaskho1 <idrox2811@gmail.com>
2020-11-14 19:06:27 -0500

    :'v

--- carrito.php
+++ carrito.php
@@ -159,7 +159,13 @@ $consultaVerificarRestaurante = 'SELECT * FROM sucursal WHERE idsucursal = ?';
                                                             $Nproducto = $producto['precio']*$producto['quantity'];  
                                                             echo number_format($Nproducto, 2, '.', ' '); } ?>
                                                     </td>
-                                                    <td class='text-center'><a href="eliminarcarrito.php?id=<?php echo $producto['idproducto']; ?>&view=<?php echo $_GET['view'] ?>"><i class="far fa-trash-alt"></i></a></td>
+                                                    <td class='text-center px-0 py-2'>
+                                                    <a href="eliminarcarrito.php?id=<?php echo $producto['idproducto']; ?>&view=<?php echo $_GET['view'] ?>">
+                                                    <button class='btn btn-danger'>
+                                                        <i class="far fa-trash-alt "></i> &nbsp; Eliminar
+                                                    </button>
+                                                    </a>
+                                                    </td>
                                             </tr>
                                         </tbody>
                                     <?php } ?>
