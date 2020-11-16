52b5c66ee9e822173d302f3903e925ecfc306aa2
arianjosafat67 <arianjosafat67@gmail.com>
2020-11-16 12:04:12 -0500

    latuya

--- eliminarcarrito.php
+++ eliminarcarrito.php
@@ -7,11 +7,18 @@
 
     if (isset($_GET['id'])) {
 
+        $producto = $conexion -> prepare('SELECT quantity FROM shop_car WHERE idproducto = ?');
+        $producto -> execute(array($_GET['id']));
+        $producto = $producto -> fetch(PDO::FETCH_ASSOC);
+
+        $insercion = $conexion -> prepare('UPDATE productos SET stock = stock + ? WHERE idproducto = ?');
+        $insercion -> execute(array($producto['quantity'],$_GET['id']));
+
         $resultados = $conexion -> prepare(
             'DELETE FROM shop_car WHERE idproducto = ?'
         );
         $resultados -> execute(array($_GET['id']));
-
+   
         if ($resultados) {
             header('Location: carrito.php?view='.$_GET['view']);
         }
