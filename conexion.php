56091fc207ba6546044dec08cb106dc51d62cc2a
larry1728 <harrietluna17@gmail.com>
2020-11-20 01:04:10 -0500

    xd

--- conexion.php
+++ conexion.php
@@ -2,7 +2,7 @@
 
     try {
         $conexion = new PDO(
-            'mysql:host=93.189.89.98;dbname=buengustoperuano;charset=UTF8',
+            'mysql:host=35.238.102.2;dbname=buengustoperuano;charset=UTF8',
             'user',
             'tuprima'
         );
