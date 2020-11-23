5c1a42fbbd804125dfe6ba6693cdcb49665fb74f
freddyjosias <fjosiashb@gmail.com>
2020-11-23 18:14:57 -0500

    f

--- conexion.php
+++ conexion.php
@@ -2,9 +2,15 @@
 
     try {
         $conexion = new PDO(
+
+            // 'mysql:host=localhost;dbname=buengustoperuano;charset=UTF8',
+            // 'root',
+            // ''
+
             'mysql:host=35.238.102.2;dbname=buengustoperuano;charset=UTF8',
             'user',
             'tuprima'
+            
         );
     } catch (Exception $e) {
         echo $e -> getMessage();
